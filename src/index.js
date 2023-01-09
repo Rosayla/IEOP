657e569e2ebdabc7478a6dcd5393c357fe47e77d
Noralira <96257729+Noralira@users.noreply.github.com>
2023-01-09 16:49:41 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import axios from 'axios'
+
+import { createOrder } from './orders';
+
+const app = express();
+
+
+app.use(express.json())
+
+app.use(async(req, res, next) => {
+    const token_request = await axios.post(
+        'https://identity.primaverabss.com/connect/token',
+        {
+            'grant_type': 'client_credentials',
+            'scope': 'application'
+        },
+        {
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            auth: {
+                username: 'IPVCIEOP',
+                password: '987fb5ac-e8bd-48da-afac-2841afc46327'
+            }
+        }
+    )
+
+    req.headers.authorization = `Bearer ${token_request.data.access_token}`
+    next()
+})
+
+app.post("/create-order", createOrder)
+
+
+
+
+
+app.listen("3000", () => console.log("http://localhost:3000"))
---
+++ src/orders.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export async function createOrder(req, res) {
+    /*const order_request = await axios.post(
+
+    )*/
+    console.log(req.body)
+
+
+
+}
